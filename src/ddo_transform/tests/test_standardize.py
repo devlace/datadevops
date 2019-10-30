0c0635417f2fcc670bbcaf44911cf651fda62bf2
Lace Lofranco <lace.lofranco@microsoft.com>
2019-10-30 17:52:36 +1100

    Add more comments to tests

--- src/ddo_transform/tests/test_standardize.py
+++ src/ddo_transform/tests/test_standardize.py
@@ -29,12 +29,14 @@ def spark():
 
 def test_standardize_parking_bay(spark):
     """Test data transform"""
+    # Arrange
     schema = standardize.get_schema("in_parkingbay_schema")
     parkingbay_sdf = spark.read.json("./data/MelbParkingBayData.json", multiLine=True, schema=schema)
     load_id = 1
     loaded_on = datetime.datetime.now()
+    # Act
     t_parkingbay_sdf, t_parkingbay_malformed_sdf = standardize.standardize_parking_bay(parkingbay_sdf, load_id, loaded_on)  # noqa: E501
-
+    # Assert
     assert t_parkingbay_sdf.count() != 0
     assert t_parkingbay_malformed_sdf.count() == 0
     assert t_parkingbay_sdf.filter(isnull("bay_id")).count() == 0
@@ -42,12 +44,14 @@ def test_standardize_parking_bay(spark):
 
 def test_standardize_sensordata(spark):
     """Test data transform"""
+    # Arrange
     schema = standardize.get_schema("in_sensordata_schema")
     sensordata_sdf = spark.read.json("./data/MelbParkingSensorData.json", multiLine=True, schema=schema)
     load_id = 1
     loaded_on = datetime.datetime.now()
+    # Act
     t_sensordata_sdf, t_sensordata_malformed_sdf = standardize.standardize_sensordata(sensordata_sdf, load_id, loaded_on)  # noqa: E501
-
+    # Assert
     assert t_sensordata_sdf.count() != 0
     assert t_sensordata_malformed_sdf.count() == 0
     assert t_sensordata_sdf.filter(isnull("bay_id")).count() == 0
